d5ee24de373d35363a3f68ce2df46c1324af5df0
Harsh Jha <harsh30jha@gmail.com>
2023-01-22 17:43:12 +0530

    app changed to function based component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { useState} from 'react'
 import Navbar from './components/Navbar';
 import Contests from './components/Contests';
 import LoadingBar from 'react-top-loading-bar'
@@ -8,40 +8,28 @@ import {
   Routes,
   Route
 } from 'react-router-dom'
-export class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      progress : 0
-    }
-  }
-  setProgress = (progress)=>{
-    this.setState({
-      progress : progress
-    })
-  }
-  render() {
+const App = ()=>{
+  const [progress,setProgress] = useState(0);
     return (
       <>
         <Router>
         <LoadingBar
         color='#f11946'
-        progress={this.state.progress}
+        progress={progress}
         height = {3}
       />
         <Navbar/>
         <Routes>
-          <Route exact path = "/" element = {<Contests setProgress = {this.setProgress}  key = "all" category = "all"/>}></Route>
-          <Route exact path = "/codeforces" element = {<Contests setProgress = {this.setProgress} key = "codeforces" category = "codeforces"/>}></Route>
-          <Route exact path = "/hacker_rank" element = {<Contests setProgress = {this.setProgress} key = "hacker_rank" category = "hacker_rank"/>}></Route>
-          <Route exact path = "/leet_code" element = {<Contests setProgress = {this.setProgress} key = "leet_code" category = "leet_code"/>}></Route>
-          <Route exact path = "/kick_start" element = {<Contests setProgress = {this.setProgress} key = "kick_start" category = "kick_start"/>}></Route>
-          <Route exact path = "/code_chef" element = {<Contests setProgress = {this.setProgress} key = "code_chef" category = "code_chef"/>}></Route>
+          <Route exact path = "/" element = {<Contests setProgress = {setProgress}  key = "all" category = "all"/>}></Route>
+          <Route exact path = "/codeforces" element = {<Contests setProgress = {setProgress} key = "codeforces" category = "codeforces"/>}></Route>
+          <Route exact path = "/hacker_rank" element = {<Contests setProgress = {setProgress} key = "hacker_rank" category = "hacker_rank"/>}></Route>
+          <Route exact path = "/leet_code" element = {<Contests setProgress = {setProgress} key = "leet_code" category = "leet_code"/>}></Route>
+          <Route exact path = "/kick_start" element = {<Contests setProgress = {setProgress} key = "kick_start" category = "kick_start"/>}></Route>
+          <Route exact path = "/code_chef" element = {<Contests setProgress = {setProgress} key = "code_chef" category = "code_chef"/>}></Route>
         </Routes>
         </Router>
       </>
     )
-  }
 }
 
 export default App
